476aa3c43cc208c8ce099de48104cc04ab60d4f6
Rémi - Le Filament <remi@le-filament.com>
2022-11-10 14:40:23 +0100

    [MIG] migration to 15.0

--- __manifest__.py
+++ __manifest__.py
@@ -2,7 +2,7 @@
     "name": "Remove login page links",
     "author": "Le Filament",
     "website": "https://le-filament.com",
-    "version": "14.0.1.0.1",
+    "version": "15.0.1.0.0",
     "license": "AGPL-3",
     "depends": ["web"],
     "data": ["views/login_template.xml"],
